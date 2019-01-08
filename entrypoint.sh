f69556aa1ee1c34988c510d2459413caabf5833b
soar1688 <coo-fly@163.com>
2019-01-08 19:55:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,5 @@ then
     sed -i "s#auth 0.0.0.0/0 - -#auth 0.0.0.0/0 - u#g" /etc/opt/ss5/ss5.conf
     sed -i "s#permit - 0.0.0.0/0 - 0.0.0.0/0 - - - - -#permit u 0.0.0.0/0 - 0.0.0.0/0 - - - - -#g" /etc/opt/ss5/ss5.conf
 fi
-#service ss5 start 
 ss5 -t -u ss5
 tail -f /var/log/ss5/ss5.log
